0e608e994e0ce6cca2eccf12135f41a9c69f1cd1
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-10-31 12:37:07 +0500

    Add project files.

---
+++ Tyuiu.KozhevnikovDG.SprintReview3.TaskReview.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KozhevnikovDG.SprintReview3.TaskReview.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KozhevnikovDG.SprintReview3.TaskReview.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("965ab0c7-23d6-4b0c-9c46-ff3023a121b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
